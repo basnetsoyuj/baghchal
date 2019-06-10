57586be1d46390145e3a9a2b4c36bf326f7a8273
SoyujBasnet <bsoyuj@gmail.com>
2019-06-10 18:59:47 +0545

    Added points_link data and improvements

--- BaghChal.py
+++ BaghChal.py
@@ -3,7 +3,10 @@ fen -> Similar to Forsyth–Edwards Notation in chess
 
 '''
 import numpy as np
+import pickle
 
+with open("data/points_link.pickle",'rb') as f:
+    connected_points_dict=pickle.load(f)
 
 class Board:
     def __init__(self, fen="B3B/5/5/5/B3B G 0"):
@@ -30,23 +33,4 @@ class Piece:
         self.position = position
 
     def connected_points(self):
-        if self.position[0] == 1:
-            y = np.array([0, 0, 0, 1])
-        elif self.position[0] == 5:
-            y = np.array([0, -1, 0, 0])
-        else:
-            y = np.array([0, -1, 0, +1])
-        if self.position[1] == 1:
-            x = np.array([1, 0, 0, 0])
-        elif self.position[1] == 5:
-            x = np.array([0, 0, -1, 0])
-        else:
-            x = np.array([1, 0, -1, 0])
-        points = connect(
-            np.array(self.position[0])+x, np.array(self.position[0])+y)-{self.position}
-        return points
-
-def connect(x,y):
-    return {(x[i],y[i]) for i in range(len(x))}
-p=Piece((1,2))
-print(p.connected_points())
+        return connected_points_dict[self.position]
